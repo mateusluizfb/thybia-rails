0462e1177076cc795971fdacb9ac7d08fc5d1434
Mateus Luiz <mateus@novatics.com.br>
2019-08-30 11:50:34 -0300

    Fix merit migrations and relations with the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
+  has_merit
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  has_merit
 end
--- spec/controllers/monsters_controller_spec.rb
+++ spec/controllers/monsters_controller_spec.rb
@@ -6,10 +6,32 @@ RSpec.describe MonstersController, type: :controller do
     let(:monster) { FactoryBot.create :monster }
     before(:each) { sign_in(user) }
 
+    subject { post :kill, params: { monster_id: monster.id } }
+
     it 'creates a new record' do
       expect {
-        post :kill, params: { monster_id: monster.id }
+        subject
       }.to change { KilledMonster.count }.from(0).to(1)
+    end
+
+    context 'User has killed 0 monsters' do
+      it "User gains a 'beginner' badge" do
+        expect {
+          subject
+        }.to change { user.reload.badges.count }.from(0).to(1)
+      end
+    end
+
+    context 'User has killed 99 monsters' do
+    end
+
+    context 'User has killed 999 monsters' do
+    end
+
+    context 'User has killed 9.999 monsters' do
+    end
+
+    context 'User has killed 99.999 monsters' do
     end
   end
 end
