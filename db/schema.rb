e7a2141d0ebcdc24b680d072d686709c40e724e6
Mateus Luiz <mateus@novatics.com.br>
2019-08-30 10:53:53 -0300

    Create CollectedCoin, Death, KillerMonster and Monster models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_30_132356) do
+ActiveRecord::Schema.define(version: 2019_08_30_135237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "collected_coins", force: :cascade do |t|
+    t.bigint "user_id"
+    t.integer "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_collected_coins_on_user_id"
+  end
+
+  create_table "deaths", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_deaths_on_user_id"
+  end
+
+  create_table "killed_monsters", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "monster_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["monster_id"], name: "index_killed_monsters_on_monster_id"
+    t.index ["user_id"], name: "index_killed_monsters_on_user_id"
+  end
+
+  create_table "monsters", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +62,8 @@ ActiveRecord::Schema.define(version: 2019_08_30_132356) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "collected_coins", "users"
+  add_foreign_key "deaths", "users"
+  add_foreign_key "killed_monsters", "monsters"
+  add_foreign_key "killed_monsters", "users"
 end
---
+++ spec/models/death_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+RSpec.describe Death, type: :model do
+end
