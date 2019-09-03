5ce85e7cf8882be4e413d0d20c86f1d32be656db
Mateus Luiz <mateus@novatics.com.br>
2019-09-03 19:19:19 -0300

    Create dynamic monster trophies

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,2 +1,17 @@
 class Monster < ApplicationRecord
+  include Merit::BadgeRulesMethods
+  after_create :create_monster_trophy
+
+  def create_monster_trophy
+    5.times do |i|
+      trophy_index = i + 1
+      count = Merit::Badge.count + 1
+
+      Merit::Badge.create!(
+        id:          count,
+        name:        "#{name} killer #{trophy_index}",
+        description: "Level #{trophy_index} monster killer"
+      )
+    end
+  end
 end
