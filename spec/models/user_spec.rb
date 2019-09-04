1c33248c6f0a1e49ec5480f905682d40bea29bcc
Mateus Luiz <mateus@novatics.com.br>
2019-09-04 16:49:21 -0300

    Add test to User#killed_amount

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe '#killed_amount' do
+    let(:user) { FactoryBot.create(:user) }
+    let(:monster) { FactoryBot.create(:monster) }
+    let!(:killed_monsters) do
+      FactoryBot.create_list(
+        :killed_monster, 10,
+        monster: monster,
+        user: user
+      )
+    end
+
+    it 'returns the amount of monsters killed for the same type' do
+      expect(user.killed_amount(monster)).to eq(10)
+    end
+  end
 end
