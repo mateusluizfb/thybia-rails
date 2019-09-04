9565fe3423076e7be02e6e7da2d787bb5b7aba9b
Mateus Luiz <mateus@novatics.com.br>
2019-09-04 17:08:55 -0300

    Refactor Monster#create_monster_trophies

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -3,16 +3,15 @@ class Monster < ApplicationRecord
   after_create :create_monster_trophies
 
   def create_monster_trophies
-    5.times do |i|
-      trophy_index = i + 1
-      count = Merit::Badge.count + 1
-
+    (1..5).inject(Merit::Badge.count + 1) do |merit_count, trophy_tier|
       Merit::Badge.create!(
-        id:          count,
+        id:          merit_count,
         multiple:    false,
-        name:        "#{name} killer #{trophy_index}",
-        description: "Level #{trophy_index} monster killer"
+        name:        "#{name} killer #{trophy_tier}",
+        description: "Level #{trophy_tier} monster killer"
       )
+
+      Merit::Badge.count + 1
     end
   end
 
