5889a8c5511a638502306d41a440f501e71e6823
Mateus Luiz <mateus@novatics.com.br>
2019-09-03 21:16:36 -0300

    Refactor User#grant_monster_badge

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,14 @@ class User < ApplicationRecord
   has_many :collected_coins
   has_many :killed_monsters
 
+  enum trophy_tier: {
+    0       => 1,
+    100     => 2,
+    1000    => 3,
+    10_000  => 4,
+    100_000 => 5
+  }
+
   def killed_amount(monster)
     KilledMonster
       .with_user(self)
@@ -17,27 +25,17 @@ class User < ApplicationRecord
       .count
   end
 
-  def grant_monster_badge(monster) # rubocop:disable Metrics/AbcSize
-    add_badge(monster.badge(1).id) if valid_for_badge?(monster, 0)
-    add_badge(monster.badge(2).id) if valid_for_badge?(monster, 100)
-    add_badge(monster.badge(3).id) if valid_for_badge?(monster, 1000)
-    add_badge(monster.badge(4).id) if valid_for_badge?(monster, 10_000)
-    add_badge(monster.badge(5).id) if valid_for_badge?(monster, 100_000)
-  end
-
   def valid_for_badge?(monster, count)
     !badge?(monster, count) && killed_amount(monster) >= count
   end
 
   def badge?(monster, count)
-    trophy_number = {
-      0       => 1,
-      100     => 2,
-      1000    => 3,
-      10_000  => 4,
-      100_000 => 5
-    }[count]
+    badges.select {|b| b.name == "#{monster.name} killer #{User.trophy_tiers[count]}" }.present?
+  end
 
-    badges.select {|b| b.name == "#{monster.name} killer #{trophy_number}" }.present?
+  def grant_monster_badge(monster)
+    User.trophy_tiers.each do |index, value|
+      add_badge(monster.badge(value).id) if valid_for_badge?(monster, index)
+    end
   end
 end
