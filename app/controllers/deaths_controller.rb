7a7d3241b037e6fe56f3abf92d945852372ae2dc
Mateus Luiz <mateus@novatics.com.br>
2019-09-04 17:23:08 -0300

    Refactor KillerMonstersController#kill

--- app/controllers/deaths_controller.rb
+++ app/controllers/deaths_controller.rb
@@ -2,7 +2,7 @@ class DeathsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    deaths = Array.new(params[:count].to_i) { Death.new(user: current_user) }
+    deaths = params[:count].to_i.times.map { Death.new(user: current_user) }
     Death.import deaths
 
     @death = deaths.last
